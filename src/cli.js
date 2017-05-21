36b2f7440066062376d51ca9b1635569ef5b1b16
Atticus White <contact@atticuswhite.com>
2017-05-21 19:03:30 -0400

    listr tasks

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const meow = require('meow');
+const chalk = require('chalk')
 const Ora = require('ora')
+const Listr = require('listr')
+const Promise = require('bluebird')
 const inquirer = require('inquirer')
 const {getCandidates} = require('./utils');
 
@@ -19,20 +22,25 @@ const cli = meow(`
   }
 });
 const prompt = inquirer.createPromptModule();
-const spinner = new Ora();
 
 function cleanNodeModules (inputPath, force) {
   const srcPath = inputPath ? path.resolve(process.cwd(), inputPath) : process.cwd()
 
-  spinner.text = 'Finding projects...'
-  spinner.start()
-  getCandidates(srcPath)
-    .then(projects => {
-      spinner.succeed(`Found ${projects.length} projects`)
-      confirmRemoval(projects)
-    }).catch(e => {
-      spinner.fail()
-    })
+  if (force) {
+    removeAll(srcPath)
+  } else {
+    const spinner = new Ora();
+    spinner.text = 'Finding projects...'
+    spinner.start()
+    getCandidates(srcPath).then(projects => {
+      if (projects.length === 0) {
+        spinner.fail('No projects found')
+      } else {
+        spinner.succeed(chalk.green(`Found ${projects.length} projects`))
+        confirmRemoval(projects)
+      }
+    }).catch(e => spinner.fail())
+  }
 }
 
 function promptMessage (project) {
@@ -44,21 +52,69 @@ function promptMessage (project) {
 }
 
 
-function confirmRemoval(candidates) {
+function confirmRemoval(projects) {
+  const project = projects[0]
+
   prompt({
     type: 'confirm',
     name: 'remove',
-    message: promptMessage(candidates[0])
+    message: promptMessage(project)
   }).then(result => {
+    const spinner = new Ora()
+
+    spinner.text = `Deleting ${project.name}`
+    spinner.start()
 
     if (result.remove) {
-      // ...
-    }
-
-    if (candidates.length > 1) {
-      confirmRemoval(candidates.slice(1))
+      setTimeout(() => {
+        spinner.succeed(chalk.green(`Deleted ${project.name}`))
+        if (projects.length > 1) {
+          confirmRemoval(projects.slice(1))
+        }
+      }, 2000)
+    } else {
+      spinner.fail(`Skipping ${project.name}`)
+      confirmRemoval(projects.slice(1))
     }
   })
+}
+
+function removeAll (path) {
+  const tasks = new Listr([
+    {
+      title: 'Finding projects',
+      task: (ctx) => getCandidates(path).then(projects => {
+        ctx.projects = projects
+        return Promise.delay(1000)
+      })
+    },
+    {
+      title: 'Cleaning projects',
+      skip: (ctx) => ctx.projects.length === 0 && 'No projects found',
+      task: (ctx) => new Listr(ctx.projects.map(project => ({
+        title: `Cleaning ${project.name}`,
+        task: () => {
+          return Promise.delay((Math.random() * (1.5 - 0.3) + 0.3) * 1000)
+        }
+      })), {concurrent: true})
+    }
+  ])
+
+  tasks.tasks[0].subscribe(event => {
+    if (event.type === 'STATE' && tasks.tasks[0].isCompleted()) {
+      tasks.tasks[0].title = `Found ${baseCtx.projects.length} projects`;
+    }
+  })
+
+  tasks.tasks[1].subscribe(event => {
+    if (event.type === 'STATE' && tasks.tasks[1].isCompleted()) {
+      tasks.tasks[1].title = `Cleaned ${baseCtx.projects.length} projects`;
+    }
+  })
+
+  const baseCtx = {projects: []};
+
+  tasks.run(baseCtx)
 }
 
 cleanNodeModules(cli.input[0], cli.flags.force);
